49975fbfa97b1ae3184800e759643c0f6a6aef18
Debanjan <debanjannaskar1@gmail.com>
2024-07-22 09:32:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const form = document.querySelector("#form");
+const rem = document.querySelector(".rem")
+const previous = document.querySelector(".prev")
+const resetButton = document.querySelector("#restart")
+const loworhi = document.querySelector(".inst")
+function initializeGame() {
+    guess = (Math.floor(Math.random() * 100)) + 1;
+    // heading.innerText = guess;
+    loworhi.innerText = "CHOOSE A NUMBER BETWEEN 1 AND 100 WITHIN 10 GUESSES"
+    prev = [];
+    attempts = 10;
+    previous.innerText = `PREVIOUS GUESSES:`;
+    rem.innerText = `CHANCES REMAINING: ${attempts}`;
+    form.querySelector("button").disabled = false;
+    form.querySelector("#input").value = "";
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const num = parseInt(document.querySelector("#input").value) ;
+    
+    if (num==guess){
+        rem.innerText = "YAYYYY you made it";
+        form.querySelector("button").disabled = true;
+        previous.innerText = "HUGE W"
+        return;
+    }
+    else{
+        if(num>guess){loworhi.innerText="HIGHER THAN THE ANSWER"}
+        else{loworhi.innerText="LOWER THAN THE ANSWER"}
+        attempts--;
+        rem.innerText = `CHANCES REMAINING: ${attempts}`;
+        prev.push(num);
+        prevstring = prev.join(" ");
+        previous.innerText = `PREVIOUS GUESSES: ${prevstring}`;
+    }
+    form.querySelector("#input").value = "";
+    if(attempts==0){
+        rem.innerText = `Game Over! The correct number was ${guess}`;
+        form.querySelector("button").disabled = true;
+        resetButton.style.display = "block";
+    }
+})  
+
+resetButton.addEventListener('click', initializeGame);
+initializeGame();
---
+++ test.js
@@ -0,0 +1,5 @@
+for (let i=1;i<=10000;i++)
+{
+    let guess = (Math.floor(Math.random()*100))+1
+    console.log(`${guess}`);    
+}
